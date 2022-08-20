d6623a5da620d5ca66fc0dd236a564b552a294ce
Waad-Awajneh <waad.awajneh707@gmail.com>
2022-08-20 20:57:35 +0300

    TASK 4 API

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Digimon-api
+ 
+ # Task four in js 
+ 1. hit the provided API and read the data from it using fetch method and create 20 object 
+     from the retrieve data then push them to an array.
+ 2. loop through the array using map and create a card for each object to render in the main .
+ 3. Add some style for the header and the footer
+ 4. Use flex or grid to organize the content of the main content.
+
+
+🔗 live link : [a link] ()
---
+++ app.js
@@ -0,0 +1,55 @@
+// function Digimon(name, image, level) {
+//   this.name = name;
+//   this.image = image;
+//   this.level = level;
+// }
+const searchButton=document.getElementById("search");
+searchButton.addEventListener('click',filterDigimonByName);
+getDigimon();
+
+function getDigimon(){
+fetch("https://digimon-api.vercel.app/api/digimon")
+  .then((data) => {
+    return data.json(); 
+  })
+  .then((dataAsObject) => {
+    const digimons = dataAsObject.filter((element, index) => {
+      return index <= 20;
+    });
+
+    displayDigimon(digimons);
+  })
+  .catch((err) => {
+    console.log('Error : cant find digimond',err);
+  });
+}
+
+function displayDigimon(digimons) {
+  let digimonToDisplay = "";
+  digimons.map((digimon) => {
+    digimonToDisplay += `<div class=card>
+        <h1 class="name">${digimon.name}</h1>
+        <img src=${digimon.img} alt="img" class="image">
+        <p>${digimon.level}<p>
+        </div>
+        `;
+    document.getElementById("cards").innerHTML = digimonToDisplay;
+  });
+}
+
+function filterDigimonByName()
+{let digimonName= document.getElementById("name").value;
+console.log(digimonName)
+    fetch(`https://digimon-api.vercel.app/api/digimon/name/${digimonName}`)
+    .then((data) => {
+      return data.json(); 
+    })
+    .then((digimons) => {
+        displayDigimon(digimons);
+    })
+    .catch((err) => {
+      console.log('Error : cant find digimond',err);
+    });
+
+
+}
